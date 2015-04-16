4c98ea93bf9b009c7996db385735d65c7d9cbba9
seungeunrho <seung_eun07@naver.com>
2015-04-16 14:54:09 +0900

    seu

--- application/controllers/login.py
+++ application/controllers/login.py
@@ -2,6 +2,8 @@
 from application import app
 from flask import Flask, redirect, url_for, render_template
 
-@app.route('/main')
-def main():
-   return render_template('main.html')
+@app.route('/')
+@app.route('/login')
+def login():
+   return render_template('login.html')
+
--- application/models/schema.py
+++ application/models/schema.py
@@ -1,4 +1,3 @@
-
 from application import db
 
 #Tables
@@ -7,3 +6,29 @@ class User(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
 	name = db.Column(db.String(40))
 	email = db.Column(db.String(80))
+	password = db.Column(db.String(60))
+
+
+class Webtoon(db.Model):
+	id = db.Column(db.Integer, primary_key = True)
+	author = db.Column(db.String(30))
+	genre = db.Column(db.String(30))
+	finished = db.Column(db.Boolean, default = '0')
+	title = db.Column(db.String(40))
+	picture = db.Column(db.String(80))
+	rating = db.Column(db.Float)
+	last_update = db.Column(db.DateTime) 
+	introduction = db.Column(db.String(150))
+
+
+# relationship tables
+
+class User_Webtoon(db.Model):
+	id 			= db.Column(db.Integer, primary_key = True)
+	user_id 	= db.Column(db.Integer, db.ForeignKey('user.id'))
+	user   		= db.relationship('User', foreign_keys = [user_id],
+		backref = db.backref('user_webtoons', cascade = 'all, delete-orphan', lazy = 'dynamic'))
+	webtoon_id 	= db.Column(db.Integer, db.ForeignKey('webtoon.id'))
+	webtoon 	= db.relationship('Webtoon', foreign_keys = [webtoon_id],
+		backref = db.backref('user_webtoons', cascade = 'all, delete-orphan', lazy = 'dynamic'))
+	score 		= db.Column(db.Integer)
