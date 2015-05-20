b2164570739aef1f5e7df82e56c821ff05b91932
seungeunrho <seung_eun07@naver.com>
2015-05-21 01:24:41 +0900

    search

--- application/models/webtoon_manager.py
+++ application/models/webtoon_manager.py
@@ -3,3 +3,6 @@ from schema import Webtoon
 
 def get_all_webtoons (con):
 	return con.execute("select id from webtoon")
+
+def get_webtoons_by_title(title):
+	return Webtoon.query.filter(Webtoon.title == title).all()
