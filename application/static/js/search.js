6b67b7fb68921d9b48b7164c7507f923006955b8
db team <abc@naver.com>
2015-05-23 02:34:01 +0000

    5star rating and ajax call for evaluation

--- application/static/js/search.js
+++ application/static/js/search.js
@@ -14,15 +14,40 @@ function get_webtoons_by_title(title) {
 	    type:'POST',
 	    data: {"title": title},
 	    success:function(response){
-		    console.log(response); 
-			$('#result').replaceWith(response);
+		console.log(response); 
+		$('#result').replaceWith(response);
+		$('.rating').raty({
+                	path: raty_img,
+			click: function(score, evt) {
+				console.log(score)
+				console.log($(this).attr('toon-id'));
+				//post_webtoon_score(user_id, $(this).attr('toon-id'), score);
+			}
+        	});
+
 	    },
 	    error: function(){
 	      	console.log("error");
 	    },
 	    complete:function(){
 	      	console.log('complete');
-	    }
-    
+           }
   	});
+}
+
+function post_webtoon_score(id, webtoon, score) {
+	$.ajax({
+		url: '/post_webtoon_score',
+		type: 'POST',
+		data: {"id": id, "webtoon": webtoon, "score": score},
+		success:function(resp) {
+			console.log(resp);
+		},
+		error: function() {
+			console.log("error");
+		},
+		complete: function() {
+			console.log("complete");
+		}
+	});
 }
