7ea2003d4e3a7c8a60b824a09bd5f0ba07e00498
Toonbox Project <toonbox@ip-172-31-0-217.ap-northeast-1.compute.internal>
2015-05-28 04:57:52 +0000

    foreign key

--- runserver.py
+++ runserver.py
@@ -1,6 +1,8 @@
 from application import app
+from application.controllers import update_similarity, recommend_webtoon
 
 # app.run()
+update_similarity.update_similarity()
+recommend_webtoon.similarity_loading()
 app.run(host = '0.0.0.0', port = 5007)
-#similarity.update_similarity()
-#recommend_webtoon.similarity_loading()
+
