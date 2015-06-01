bceddcdcf3aca939e4338c451ff0997de997a1ba
Toonbox Project <toonbox@ip-172-31-0-217.ap-northeast-1.compute.internal>
2015-06-01 12:30:15 +0000

    load sim on runserver

--- runserver.py
+++ runserver.py
@@ -3,6 +3,6 @@ from application.controllers import update_similarity, recommend_webtoon
 
 # app.run()
 # update_similarity.update_similarity()
-# recommend_webtoon.similarity_loading()
+recommend_webtoon.similarity_loading()
 app.run(host = '0.0.0.0', port = 5007)
 
